c25288757e7ef84861f30bc6d83bcd251ec42746
trumreup98 <117432227+trumreup98@users.noreply.github.com>
2023-09-25 11:52:25 +0700

    Update px-update-wan127-arm64-ubuntu.sh

--- px-update-wan127-arm64-ubuntu.sh
+++ px-update-wan127-arm64-ubuntu.sh
@@ -1,4 +1,4 @@
-curl -SL -o wan-proxy-release.zip https://github.com/trumreup98/zip/raw/main/bc-wan-proxy-v127-update.zip
+curl -SL -o wan-proxy-release.zip https://github.com/trumreup98/zip/raw/main/eg-wan-proxy-v127-update.zip
 sudo unzip wan-proxy-release.zip -d /var/www/eg_proxy
 sudo systemctl restart eager-proxy.service
 rm wan-proxy-release.zip
